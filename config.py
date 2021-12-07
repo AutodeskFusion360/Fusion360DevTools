f537c07d8b2c475d42d25cec475fc4ebeb55cd7e
Patrick Rainsberry <patrick.rainsberry@gmail.com>
2021-12-06 16:59:29 -0800

    Clean Up

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-import os
-
 DEBUG = True
 
 # ADDIN_NAME = os.path.basename(os.path.dirname(__file__))
 ADDIN_NAME = "DEV TOOLS"
 COMPANY_NAME = "Autodesk"
 
+# Name for a directory in user home to store data
 user_dir_name = f'{ADDIN_NAME}'
 
 # Design Workspace
@@ -20,13 +19,9 @@ attributes_panel_name = 'Attributes'
 attributes_panel_id = f'{ADDIN_NAME}_attributes_panel'
 attributes_panel_after = ''
 
-geometry_panel_name = 'Geometry'
-geometry_panel_id = f'{ADDIN_NAME}_geometry_panel'
-geometry_panel_after = attributes_panel_id
-
 data_panel_name = 'Data'
 data_panel_id = f'{ADDIN_NAME}_data_panel'
-data_panel_after = geometry_panel_id
+data_panel_after = attributes_panel_id
 
 dev_panel_name = 'API Tools'
 dev_panel_id = f'{ADDIN_NAME}_dev_panel'
@@ -39,6 +34,7 @@ ui_palette_id = f'{ADDIN_NAME}_ui_palette'
 api_palette_name = 'API Explorer'
 api_palette_id = f'{ADDIN_NAME}_api_palette'
 
+# Reference for use in some commands
 all_workspace_names = [
     'FusionSolidEnvironment', 'GenerativeEnvironment', 'PCBEnvironment', 'PCB3DEnvironment', 'Package3DEnvironment',
     'FusionRenderEnvironment', 'Publisher3DEnvironment', 'SimulationEnvironment', 'CAMEnvironment', 'DebugEnvironment',
